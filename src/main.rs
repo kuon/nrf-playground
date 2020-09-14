f14a6b2fe59d96e51752a9af7af96e5b576941b9
Nicolas goy <kuon@goyman.com>
2020-09-14 12:57:54 +0200

    Try more things

--- src/main.rs
+++ src/main.rs
@@ -28,16 +28,65 @@ use defmt::debug;
 
 #[app(device = nrf52840_hal::pac, peripherals = true)]
 const APP: () = {
-    /*
     struct Resources {
         serial: Uarte<UARTE0>,
         timer: Timer<TIMER1, Periodic>,
     }
+
+    #[init(spawn = [softdevice])]
+    fn init(mut cx: init::Context) -> init::LateResources {
+        debug!("Starting up");
+
+        /*
+        // Enable external crystal
+        let _clocks =
+            hal::clocks::Clocks::new(cx.device.CLOCK).enable_ext_hfosc();
+        */
+
+        let p0 = hal::gpio::p0::Parts::new(cx.device.P0);
+
+        let mut serial = Uarte::new(
+            cx.device.UARTE0,
+            Pins {
+                txd: p0.p0_06.into_push_pull_output(Level::High).degrade(),
+                rxd: p0.p0_08.into_floating_input().degrade(),
+                cts: None,
+                rts: None,
+            },
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+
+        debug!("Start init");
+        writeln!(serial, "\n--- INIT ---\r\n").unwrap();
+
+
+        cx.core.DCB.enable_trace();
+        cx.core.DWT.enable_cycle_counter();
+
+        let mut timer = Timer::new(cx.device.TIMER1);
+        timer.enable_interrupt();
+        let mut timer = timer.into_periodic();
+        timer.start(1_000_000u32); // 1Mhz, so once per second
+
+        writeln!(serial, "\n--- END --- \r\n").unwrap();
+
+        cx.spawn.softdevice().unwrap();
+        debug!("End init");
+
+        init::LateResources { serial, timer }
+    }
+
+    /*
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        debug!("Running idle");
+        loop {}
+    }
     */
 
-    #[init()]
-    fn init(mut cx: init::Context) { //-> init::LateResources {
-        debug!("Starting up");
+    #[task]
+    fn softdevice(cx: softdevice::Context) {
         let config = nrf_softdevice::Config {
             clock: Some(raw::nrf_clock_lf_cfg_t {
                 source: raw::NRF_CLOCK_LF_SRC_XTAL as u8,
@@ -76,54 +125,16 @@ const APP: () = {
         let sd = Softdevice::enable(&config);
 
         debug!("Soft device created");
-
-        // Enable external crystal
-
-        /*
-        let _clocks =
-            hal::clocks::Clocks::new(cx.device.CLOCK).enable_ext_hfosc();
-        */
-
-        /*
-        let p0 = hal::gpio::p0::Parts::new(cx.device.P0);
-
-        let mut serial = Uarte::new(
-            cx.device.UARTE0,
-            Pins {
-                txd: p0.p0_06.into_push_pull_output(Level::High).degrade(),
-                rxd: p0.p0_08.into_floating_input().degrade(),
-                cts: None,
-                rts: None,
-            },
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        );
-
-        writeln!(serial, "\n--- INIT ---\r\n").unwrap();
-
-        cx.core.DCB.enable_trace();
-        cx.core.DWT.enable_cycle_counter();
-
-        let mut timer = Timer::new(cx.device.TIMER1);
-        timer.enable_interrupt();
-        let mut timer = timer.into_periodic();
-        timer.start(1_000_000u32); // 1Mhz, so once per second
-
-        writeln!(serial, "\n--- END --- \r\n").unwrap();
-
-        init::LateResources { serial, timer }
-        */
-    }
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        loop {}
     }
 
-    /*
     #[task(binds = TIMER1, resources = [serial, timer])]
     fn exec(cx: exec::Context) {
         cx.resources.timer.wait().unwrap();
+        debug!("Timer exec");
         writeln!(cx.resources.serial, "\n--- TIMER EXEC ---\r\n").unwrap();
     }
-    */
+
+    extern "C" {
+        fn SWI0_EGU0();
+    }
 };
